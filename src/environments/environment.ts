6d08f7005818ead2a4d745b1018348066e76323c
Marcos lopez <marcosmatiaslopez.2018@gmail.com>
2026-01-08 10:27:51 -0300

    changed environment to desa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  API_URL:'https://api.cirsubgn.org.ar',
+  API_URL:'http://localhost:3000',
 };
+/* export const environment = {
+  API_URL:'https://api.cirsubgn.org.ar',
+}; */
 
